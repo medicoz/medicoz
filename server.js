efa34ee7c378f44527ec2b4417ca00f86d333659
SatyamSingh92 <satyamsingh1991@gmail.com>
2016-06-22 21:02:38 +0530

    db url added

--- server.js
+++ server.js
@@ -47,3 +47,19 @@ var server = http.createServer(function(request, response) {
   var absPath = "./" + filePath;
   serverWorking(response, absPath);
 }).listen(4000)  
+
+
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+
+MongoClient.connect('mongodb://localhost/ptest', function(err, db) {
+    if(err) throw err;
+
+    var collection = db.collection('test_insert');
+    collection.insert({a:2}, function(err, docs) {
+        collection.count(function(err, count) {
+            console.log(format("count = %s", count));
+            db.close();
+        });
+    });
+});
